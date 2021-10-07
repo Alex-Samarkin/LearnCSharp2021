d4bf518070ffd4e8398602db77d570ab140ed8f1
Alex-Samarkin <alexsamarkinru@gmail.com>
2021-10-07 20:11:41 +0300

    плюс угадай число

---
+++ Tut3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tut3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Игра 'Угадай число'
+я загадаю число от 0 до 100, вы попробуете угадать. 
+Я отвечу только больше ваше число моего или меньше.
+");
+
+            Console.WriteLine("Ваше число?");
+
+            Console.ReadLine();
+
+        }
+    }
+}
