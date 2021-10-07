da18f83ad861028b7fc13ec9ba985aa18bf5148a
alex_ <alexsamarkinru@gmail.com>
2021-10-07 13:32:01 +0300

    вод числа

--- Tut2/Program.cs
+++ Tut2/Program.cs
@@ -18,6 +18,26 @@ namespace Tut2
 
             Console.ReadLine();
 
+            Console.WriteLine("Введите число");
+            string value = Console.ReadLine();
+            // лучше проверить, что все ОК
+            /*
+             * if(value == "") value = "0.0";
+             */
+            
+            double x = Double.Parse(value);
+            // лучше проверить, что преобразование прошло нормально
+
+            Console.WriteLine($"x= {x} x^2= {x*x} x^3= {x*x*x} x^4= {x*x*x*x}");
+            Console.WriteLine($"1/x= {1/x} 1/x^2= {1/(x * x)} 1/x^3= {1/(x * x * x)} 1/x^4= {1/(x * x * x * x)}");
+
+            // добавьте необходимое
+            Console.WriteLine("Если это радиус:");
+            Console.WriteLine("Длина окружности {Math.PI*x} Площадь круга {} Объем сферы {} ");
+
+            Console.ReadLine();
+
+
 
         }
     }
