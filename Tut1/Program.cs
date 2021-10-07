19caca6e89c5ca34fb7c729abd321de47a207b0b
alex_ <alexsamarkinru@gmail.com>
2021-10-07 11:27:53 +0300

    Плюс переменные

---
+++ Tut1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tut1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Простой вывод
+            Console.WriteLine("Hello, World, Привет мир!");
+
+            //Ввод
+            Console.ReadLine();
+
+            //две переменные
+            int x = 0;
+            int y = 100;
+
+            //вывод
+            Console.WriteLine("x= {0} y= {1} x+y= {2}",x,y,x+y);
+            Console.ReadLine();
+        }
+    }
+}
